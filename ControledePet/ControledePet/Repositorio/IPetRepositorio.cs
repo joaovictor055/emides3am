f6055d9dcd4ccce38eae562f4d4398516dc50a53
joao victor <joaovictorsantanavieira147@gmail.com>
2023-08-25 16:11:21 -0300

    crud

---
+++ ControledePet/ControledePet/Repositorio/IPetRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using ControledePet.Models;
+
+namespace ControledePet.Repositorio
+{
+    public interface IPetRepositorio
+    {
+        List<PetModel> BuscarTodos();
+        PetModel ListarPorId(int id);
+        PetModel Adicionar(PetModel pet);
+        PetModel Atualizar(PetModel pet);
+        bool Apagar(int id);
+    }
+}
